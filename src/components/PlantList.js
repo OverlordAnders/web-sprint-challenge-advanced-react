ee100fcb12403d3e78ee306ff9d466a860611fbd
Brina <53455199+OverlordAnders@users.noreply.github.com>
2021-10-01 10:42:28 -0400

    Update PlantList.js

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -10,6 +10,16 @@ export default class PlantList extends Component {
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
+  componentDidMount(){
+    axios.get(`http://localhost:3333/plants`)
+      .then(resp=> {
+        console.log(resp)
+        this.setState({
+          plants: resp.data
+        })
+      })
+  }
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
